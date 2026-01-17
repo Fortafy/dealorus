a2ae8c0a8e14f9d533ce146921527c32bdbc44a1
Sean Ramsey <sean@fortafy.us>
2026-01-17 23:10:16 +0000

    File changes

--- src/components/contacts/EnrichContactDialog.jsx
+++ src/components/contacts/EnrichContactDialog.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
