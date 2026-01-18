88d869dbc32963adf901301bf4f51fb8f6c62f07
Sean Ramsey <sean@fortafy.us>
2026-01-18 03:17:42 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,15 +1,11 @@
-import Home from './pages/Home';
 import Organizations from './pages/Organizations';
-import Dashboard from './pages/Dashboard';
 
 
 export const PAGES = {
-    "Home": Home,
     "Organizations": Organizations,
-    "Dashboard": Dashboard,
 }
 
 export const pagesConfig = {
-    mainPage: "Home",
+    mainPage: "Organizations",
     Pages: PAGES,
 };
