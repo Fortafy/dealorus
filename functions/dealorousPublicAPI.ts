dbf8bfde0aab40430b0e6e3769cd4ebf023c4838
Sean Ramsey <sean@fortafy.us>
2026-03-06 22:50:00 +0000

    File changes

--- functions/dealorousPublicAPI.ts
+++ functions/dealorousPublicAPI.ts
@@ -39,7 +39,7 @@ function buildOpenApiSchema(baseUrl) {
     },
     servers: [{ url: baseUrl }],
     paths: {
-      "/": {
+      "/dealorousPublicAPI": {
         post: {
           operationId: "searchNonprofits",
           summary: "Search for nonprofit organizations",
