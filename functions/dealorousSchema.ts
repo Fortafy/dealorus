66d6d037b3564fec837b3eea1da9bebb2b4a5b17
Sean Ramsey <sean@fortafy.us>
2026-03-06 23:31:33 +0000

    File changes

--- functions/dealorousSchema.ts
+++ functions/dealorousSchema.ts
@@ -19,7 +19,7 @@ Deno.serve(async (req) => {
     },
     servers: [{ url: "https://civic-beacon-acaf302c.base44.app" }],
     paths: {
-      "/dealorousPublicAPI": {
+      "/api/functions/dealorousPublicAPI": {
 
         post: {
           operationId: "searchNonprofits",
